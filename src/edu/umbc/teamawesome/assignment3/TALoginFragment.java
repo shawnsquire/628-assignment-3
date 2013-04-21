f8b0628a685d1bc24a9542a334d46fb2b716118e
Justin Ermer <justin.ermer@gmail.com>
2013-04-20 22:49:48 -0400

    added timer for location updates, added markers for map, corrected webservice bugs

--- src/edu/umbc/teamawesome/assignment3/TALoginFragment.java
+++ src/edu/umbc/teamawesome/assignment3/TALoginFragment.java
@@ -5,11 +5,15 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -116,9 +120,12 @@ public class TALoginFragment extends Fragment
         return true;
     }
 
-    private void login(String username, String password)
+    private void login(final String username, String password)
     {
     	isLoading(true);
+    	
+	    final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+	    imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
     	
     	TAWebService.authenticate(username, password, getActivity(), new TAWebServiceDelegate() 
     	{
@@ -127,6 +134,8 @@ public class TALoginFragment extends Fragment
 			public void webServiceDidFinishWithResult(Object result) 
 			{
 				isLoading(false);
+				TAUserPreferences.setCurrentUser(getActivity(), (TAUser) result);
+
 				delegate.userDidLogin();
 				Log.i("WEBSERVICE", "success");
 			}
@@ -139,7 +148,7 @@ public class TALoginFragment extends Fragment
 	        	AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()); 
 	        	builder.setMessage(R.string.login_failed).setTitle("Error");
 	        	builder.create().show();
-				Log.i("WEBSERVICE", errorString);
+				Log.i("WEBSERVICE", "authenticate did fail with error: " + errorString);
 			}
 		});
     	
--- src/edu/umbc/teamawesome/assignment3/TAUser.java
+++ src/edu/umbc/teamawesome/assignment3/TAUser.java
@@ -4,7 +4,43 @@ public class TAUser {
 
 	protected String username;
 	protected String userId;
+	protected String time;
+	protected String latitude;
+	protected String longitude;
+	protected String firstname;
+	protected String lastname;
+
 	
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}	
 	public String getUsername() {
 		return username;
 	}
@@ -16,6 +52,11 @@ public class TAUser {
 	}
 	public void setUserId(String userId) {
 		this.userId = userId;
+	}
+	
+	public String toString()
+	{
+		return getUsername() + ": " + getUserId() + ": " + getLatitude() + "-" + getLongitude();
 	}
 	
 }
